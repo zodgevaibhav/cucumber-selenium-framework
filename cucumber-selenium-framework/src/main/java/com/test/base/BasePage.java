54c1a00035f8218b63629743f4ccf6c414ba1bde
Govinda Revanwar <govindarevanwar@m-c02yp6zxlvcg.lan>
2020-05-03 17:57:01 +0530

    updated method name

--- cucumber-selenium-framework/src/main/java/com/test/base/BasePage.java
+++ cucumber-selenium-framework/src/main/java/com/test/base/BasePage.java
@@ -9,17 +9,18 @@ import com.test.webdriver_factory.WebDriverFactory;
 public class BasePage {
 	protected WebDriver driver;
 	protected SoftAssert softAssert;
-
+	protected Wait<WebDriver> fluentwait;
 	public ExtentLogger logger = new ExtentLogger();
 	public BasePage()
 	{
 		driver = WebDriverFactory.getDriver();
 		PageFactory.initElements(driver, this);
+		fluentwait = getFluentWaitObject(15);
 	}
 	
 	public Wait<WebDriver> CustomFluentWait(int timeout, int pollingSeconds)
 	{
-		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+		Wait<WebDriver> wait = new getFluentWaitObject<WebDriver>(driver)
 			    .withTimeout(Duration.ofSeconds(timeout)) 
 			    .pollingEvery(Duration.ofSeconds(pollingSeconds)); 
 		return wait;
