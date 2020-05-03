a51eab1cb14ca02762d749414bf61e1def106d40
sherinmathew <sherinwip@gmail.com>
2020-05-03 23:55:05 +0530

    DB support added to framework

--- cucumber-selenium-framework/src/main/java/com/test/base/BasePage.java
+++ cucumber-selenium-framework/src/main/java/com/test/base/BasePage.java
@@ -1,12 +1,17 @@
 package com.test.base;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import com.suite.commons.DBUtils;
 import com.suite.commons.ExtentLogger;
+import com.suite.commons.database.DBConnection;
 import com.test.test_data.RuntimeTestDataHolder;
 import com.test.webdriver_factory.WebDriverFactory;
 /**
@@ -17,7 +22,7 @@ public class BasePage {
 	protected WebDriver driver;
 	public ExtentLogger logger = new ExtentLogger();
 	private Map<String,String> runTimeTestData = new HashMap<String,String>();
-	
+
 	public BasePage()
 	{
 		driver = WebDriverFactory.getDriver();
@@ -30,12 +35,59 @@ public class BasePage {
 	}
 
 	public void setRunTimeTestData(String key, String value) {
-		
+
 		if(runTimeTestData.containsKey(key))
 		{
 			logger.info("!!!!!!!!!! Duplicate Runtime Data Key inserstion found. Key "+key+" was already present in Map with value "+runTimeTestData.get(key)+". Same key is being asked to insert again, so, overwriting it with new value "+value+". Confirm if it is intended.");
 		}
 		runTimeTestData.put(key, value);
+	}
+
+	/**
+	 * Method executeSelectQuery is used to executing select queries
+	 * @param String query to be provided with the desired select query
+	 * 
+	 * */    
+	public ResultSet executeSelectQuery(String query){
+
+		ResultSet result= null;
+
+		try {
+			DBConnection dbconn = DBUtils.getDBConnection();
+			Connection conn = dbconn.getConn();
+			result = conn.createStatement().executeQuery(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return result;
+
+
+
+	}
+
+	
+	/**
+	 * Method executeUpdateOrDeleteQuery will be used to perform update or delete DB opertion
+	 * 
+	 * 
+	 * **/
+	public int executeUpdateOrDeleteQuery(String query){
+		int resultCount=0;
+		try {
+			DBConnection dbconn = DBUtils.getDBConnection();
+			Connection conn = dbconn.getConn();
+			resultCount = conn.createStatement().executeUpdate(query);
+			logger.info("****Result count "+resultCount);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resultCount;
 	}
 
 }
