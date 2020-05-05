07878d9eee565da4b86b3d57d73b7a96af80aa37
zodgevaibhav <zodgevaibhav@gmail.com>
2020-05-05 11:26:21 +0530

    Plugged test project in framework

---
+++ cucumber-selenium-framework/src/test/java/com/orangehrm/pages/HomePage.java
@@ -0,0 +1,25 @@
+package com.orangehrm.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+import com.test.base.BasePage;
+
+import io.cucumber.java.en.Then;
+
+public class HomePage extends BasePage {
+
+	//BasePage provides driver object, logger object
+	
+	@FindBy(id = "welcome")
+	private WebElement welComeText;
+
+	@Then("user should be able to see {string} message")
+	public HomePage verifyWelcomeText(String strWelComeText) {
+		logger.info("********** In verify welcome message method");
+		logger.info("********** Run time data stored check - Title "+getRunTimeTestData("BrowserTitle"));
+		Assert.assertEquals(welComeText.getText(), strWelComeText);
+		return this;
+	}
+}
---
+++ cucumber-selenium-framework/src/test/java/com/orangehrm/pages/LoginPage.java
@@ -0,0 +1,48 @@
+package com.orangehrm.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.suite.commons.PropertyHolder;
+import com.test.base.BasePage;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class LoginPage extends BasePage{
+	
+	private static final Logger logger = LoggerFactory.getLogger(LoginPage.class);
+
+	
+	@FindBy(id="txtUsername")
+	private WebElement userName;
+	
+	@FindBy(id="txtPassword")
+	private WebElement password;
+	
+	@FindBy(id="btnLogin")
+	private WebElement loginButton;
+	
+	
+	
+	@Given("user navigate to orange hrm URL")
+	public LoginPage navigateToOrangeHrmApplication()
+	{
+		String ENVIRONMENT = PropertyHolder.generalProperties.getProperty("ENVIRONMENT");
+		driver.get(PropertyHolder.generalProperties.getProperty(ENVIRONMENT+"_URL"));
+		setRunTimeTestData("BrowserTitle", driver.getTitle());
+		return this;
+	}
+	
+	@When("uses enters user name as {string} and password as {string} and click on login button")
+	public void loginForSuccess(String userName, String password)
+	{
+		logger.info("********** In verify welcome message method");
+		this.userName.sendKeys(userName);
+		this.password.sendKeys(password);
+		this.loginButton.click();
+	}
+
+}
